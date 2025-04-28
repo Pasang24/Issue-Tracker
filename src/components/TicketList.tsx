7b62410ed7701b6786b84e542108135ce297765e
Pasang24 <pasangtheeng14@gmail.com>
2025-04-28 20:58:04 +0545

    feat: show a illustration for empty ticket list

--- src/components/TicketList.tsx
+++ src/components/TicketList.tsx
@@ -1,16 +1,27 @@
 import { Ticket } from "@/types/Ticket";
 import TicketCard from "./TicketCard";
+import NoTicket from "@/illustrations/NoTicket";
 
 interface TicketListProps {
   tickets: Ticket[];
 }
 
 async function TicketList({ tickets }: TicketListProps) {
+  if (tickets.length)
+    return (
+      <div className="flex flex-col gap-2 my-4">
+        {tickets.map((ticket) => (
+          <TicketCard ticket={ticket} key={ticket.id} />
+        ))}
+      </div>
+    );
+
   return (
-    <div className="flex flex-col gap-2 my-4">
-      {tickets.map((ticket) => (
-        <TicketCard ticket={ticket} key={ticket.id} />
-      ))}
+    <div className="max-w-[400px] mx-auto my-4">
+      <NoTicket />
+      <p className="text-center">
+        Hooray! It seems we don't have any issue here.
+      </p>
     </div>
   );
 }
