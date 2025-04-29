d2076cd10dfb7f7ca6346425b40447e37e0f8a89
Pasang24 <pasangtheeng14@gmail.com>
2025-04-29 13:20:20 +0545

    feat: add auth guard using middlware to protect routes from unauthenticated users

---
+++ src/middleware.ts
@@ -0,0 +1,55 @@
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+const publicRoutes = ["/login", "/signup", "/forgot-password"];
+const protectedRoutes = [
+  "/tickets",
+  "/reported-tickets",
+  "/assigned-tickets",
+  "create-ticket",
+];
+
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+  const currentRoute = request.nextUrl.pathname;
+
+  const isPublicRoute = publicRoutes.some((route) =>
+    currentRoute.startsWith(route)
+  );
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    currentRoute.startsWith(route)
+  );
+
+  if (!token && isProtectedRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  let isAuthenticated = false;
+
+  if (token) {
+    try {
+      await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
+
+      isAuthenticated = true;
+    } catch (error) {
+      return NextResponse.redirect(
+        new URL("/login", request.url)
+      ).cookies.delete("token");
+    }
+  }
+
+  if (currentRoute === "/") {
+    if (token) {
+      return NextResponse.redirect(new URL("/tickets", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  if (isPublicRoute && token) {
+    return NextResponse.redirect(new URL("/tickets", request.url));
+  }
+
+  return NextResponse.next();
+}
